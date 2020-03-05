1b16b8176339d818806a118f37ecbbd19db8cfe1
th3735da-s <th3735da-s@student.lu.se>
2020-03-05 12:28:19 +0100

    GUI complete
    Complete with shell of GUI.

--- src/application/Application.java
+++ src/application/Application.java
@@ -7,6 +7,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JSpinner;
@@ -15,12 +16,12 @@ import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import java.awt.Font;
+import java.awt.Color;
 
 public class Application {
 
 	private JFrame frame;
 	private JTextField textFieldRegisterInvoiceNumber;
-	private JTextField textFieldRegisterNewInvoiceProductNumber;
 	private JTextField textFieldNewCategoryName;
 	private JTextField textFieldNewProductName;
 	private JTextField textFieldFindInvoiceByInvoiceNumber;
@@ -29,11 +30,17 @@ public class Application {
 	private JTextField textFieldNewSupplierNumber;
 	private JTextField textFieldNewSupplierSite;
 	private JLabel lblRemoveProductName;
+	private JTextField textFieldNewProductNumber;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -86,13 +93,9 @@ public class Application {
 		lblRegisterAmount.setBounds(10, 240, 25, 14);
 		panelNewInvoice.add(lblRegisterAmount);
 		
-		JLabel lblRegisterProductNumber = new JLabel("Varunummer");
-		lblRegisterProductNumber.setBounds(10, 215, 87, 14);
-		panelNewInvoice.add(lblRegisterProductNumber);
-		
-		JLabel lblRegisterProductName = new JLabel("Namn");
-		lblRegisterProductName.setBounds(10, 265, 48, 14);
-		panelNewInvoice.add(lblRegisterProductName);
+		JLabel lblRegisterProduct = new JLabel("Vara");
+		lblRegisterProduct.setBounds(10, 215, 87, 14);
+		panelNewInvoice.add(lblRegisterProduct);
 		
 		JLabel lblRegisterInvoiceCopy = new JLabel("Bifoga kopia");
 		lblRegisterInvoiceCopy.setBounds(10, 138, 87, 14);
@@ -123,18 +126,13 @@ public class Application {
 		btnRegisterInvoiceChooseFile.setBounds(107, 136, 96, 19);
 		panelNewInvoice.add(btnRegisterInvoiceChooseFile);
 		
-		textFieldRegisterNewInvoiceProductNumber = new JTextField();
-		textFieldRegisterNewInvoiceProductNumber.setBounds(107, 212, 96, 20);
-		panelNewInvoice.add(textFieldRegisterNewInvoiceProductNumber);
-		textFieldRegisterNewInvoiceProductNumber.setColumns(10);
-		
 		JSpinner spinnerRegisterNewInvoiceAmount = new JSpinner();
 		spinnerRegisterNewInvoiceAmount.setBounds(107, 237, 96, 20);
 		panelNewInvoice.add(spinnerRegisterNewInvoiceAmount);
 		
-		JComboBox comboBoxRegisterInvoiceProductName = new JComboBox();
-		comboBoxRegisterInvoiceProductName.setBounds(107, 263, 96, 18);
-		panelNewInvoice.add(comboBoxRegisterInvoiceProductName);
+		JComboBox comboBoxRegisterInvoiceProduct = new JComboBox();
+		comboBoxRegisterInvoiceProduct.setBounds(107, 213, 96, 18);
+		panelNewInvoice.add(comboBoxRegisterInvoiceProduct);
 		
 		JLabel lblRegisterReceivedDate = new JLabel("Mottaget datum");
 		lblRegisterReceivedDate.setBounds(10, 63, 87, 14);
@@ -149,7 +147,7 @@ public class Application {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnRegisterInvoice.setBounds(10, 314, 193, 23);
+		btnRegisterInvoice.setBounds(10, 280, 193, 23);
 		panelNewInvoice.add(btnRegisterInvoice);
 		
 		JPanel panelSearchInvoice = new JPanel();
@@ -198,140 +196,19 @@ public class Application {
 		
 		JPanel panelRemoveInvoice = new JPanel();
 		tabbedPaneInsideInvoice.addTab("Ta Bort Faktura", null, panelRemoveInvoice, null);
+		panelRemoveInvoice.setLayout(null);
 		
-		JPanel panelCategory = new JPanel();
-		tabbedPane.addTab("Kategorier och Varor", null, panelCategory, null);
-		panelCategory.setLayout(null);
+		JLabel lblInvoiceNumber = new JLabel("Fakturanummer");
+		lblInvoiceNumber.setBounds(10, 11, 102, 14);
+		panelRemoveInvoice.add(lblInvoiceNumber);
 		
-		JTabbedPane tabbedPaneInsideCategory = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPaneInsideCategory.setBounds(10, 11, 822, 489);
-		panelCategory.add(tabbedPaneInsideCategory);
+		JComboBox comboBoxRemoveInvoice = new JComboBox();
+		comboBoxRemoveInvoice.setBounds(133, 11, 102, 18);
+		panelRemoveInvoice.add(comboBoxRemoveInvoice);
 		
-		JPanel panelRegisterNewCategory = new JPanel();
-		tabbedPaneInsideCategory.addTab("Ny Kategory eller Vara", null, panelRegisterNewCategory, null);
-		panelRegisterNewCategory.setLayout(null);
-		
-		JLabel lblNewCategory = new JLabel("Ny kategori: ");
-		lblNewCategory.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewCategory.setBounds(10, 11, 79, 14);
-		panelRegisterNewCategory.add(lblNewCategory);
-		
-		JLabel lblNewCategoryName = new JLabel("Namn");
-		lblNewCategoryName.setBounds(10, 36, 48, 14);
-		panelRegisterNewCategory.add(lblNewCategoryName);
-		
-		textFieldNewCategoryName = new JTextField();
-		textFieldNewCategoryName.setBounds(78, 33, 96, 20);
-		panelRegisterNewCategory.add(textFieldNewCategoryName);
-		textFieldNewCategoryName.setColumns(10);
-		
-		JButton btnAddCategory = new JButton("L\u00E4gg till kategori");
-		btnAddCategory.setBounds(10, 61, 164, 23);
-		panelRegisterNewCategory.add(btnAddCategory);
-		
-		JLabel lblNewProduct = new JLabel("Ny vara:");
-		lblNewProduct.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewProduct.setBounds(10, 120, 48, 14);
-		panelRegisterNewCategory.add(lblNewProduct);
-		
-		JLabel lblNewProductName = new JLabel("Namn");
-		lblNewProductName.setBounds(10, 145, 48, 14);
-		panelRegisterNewCategory.add(lblNewProductName);
-		
-		JLabel lblNewProductCategory = new JLabel("Kategori");
-		lblNewProductCategory.setBounds(10, 170, 48, 14);
-		panelRegisterNewCategory.add(lblNewProductCategory);
-		
-		JComboBox comboBoxNewProductCategory = new JComboBox();
-		comboBoxNewProductCategory.setBounds(78, 170, 96, 18);
-		panelRegisterNewCategory.add(comboBoxNewProductCategory);
-		
-		textFieldNewProductName = new JTextField();
-		textFieldNewProductName.setBounds(78, 142, 96, 20);
-		panelRegisterNewCategory.add(textFieldNewProductName);
-		textFieldNewProductName.setColumns(10);
-		
-		JButton btnAddProduct = new JButton("L\u00E4gg till vara");
-		btnAddProduct.setBounds(10, 195, 164, 23);
-		panelRegisterNewCategory.add(btnAddProduct);
-		
-		JPanel panelRemoveCategory = new JPanel();
-		tabbedPaneInsideCategory.addTab("Ta Bort Kategori eller Vara", null, panelRemoveCategory, null);
-		panelRemoveCategory.setLayout(null);
-		
-		JLabel lblRemoveCategory = new JLabel("Kategori: ");
-		lblRemoveCategory.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblRemoveCategory.setBounds(17, 11, 99, 14);
-		panelRemoveCategory.add(lblRemoveCategory);
-		
-		JLabel lblRemoveCategoryName = new JLabel("Namn");
-		lblRemoveCategoryName.setBounds(17, 36, 48, 14);
-		panelRemoveCategory.add(lblRemoveCategoryName);
-		
-		JComboBox comboBoxRemoveCategoryName = new JComboBox();
-		comboBoxRemoveCategoryName.setBounds(85, 36, 99, 18);
-		panelRemoveCategory.add(comboBoxRemoveCategoryName);
-		
-		JLabel lblRemoveProduct = new JLabel("Vara:");
-		lblRemoveProduct.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblRemoveProduct.setBounds(17, 99, 73, 14);
-		panelRemoveCategory.add(lblRemoveProduct);
-		
-		lblRemoveProductName = new JLabel("Namn");
-		lblRemoveProductName.setBounds(17, 124, 48, 14);
-		panelRemoveCategory.add(lblRemoveProductName);
-		
-		JComboBox comboBoxRemoveProductName = new JComboBox();
-		comboBoxRemoveProductName.setBounds(85, 124, 99, 18);
-		panelRemoveCategory.add(comboBoxRemoveProductName);
-		
-		JButton btnTaBort_1 = new JButton("Ta Bort");
-		btnTaBort_1.setBounds(17, 153, 89, 23);
-		panelRemoveCategory.add(btnTaBort_1);
-		
-		JLabel lblEller = new JLabel("Eller");
-		lblEller.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblEller.setBounds(17, 74, 48, 14);
-		panelRemoveCategory.add(lblEller);
-		
-		JPanel panelReport = new JPanel();
-		tabbedPane.addTab("Rapport", null, panelReport, null);
-		panelReport.setLayout(null);
-		
-		JScrollPane scrollPaneReport = new JScrollPane();
-		scrollPaneReport.setBounds(388, 11, 444, 489);
-		panelReport.add(scrollPaneReport);
-		
-		JTextArea textAreaReport = new JTextArea();
-		scrollPaneReport.setViewportView(textAreaReport);
-		
-		JLabel lblReportChooseDate = new JLabel("V\u00E4lj datum");
-		lblReportChooseDate.setBounds(10, 17, 72, 14);
-		panelReport.add(lblReportChooseDate);
-		
-		JLabel lblReportDateTo = new JLabel("till");
-		lblReportDateTo.setBounds(165, 17, 16, 14);
-		panelReport.add(lblReportDateTo);
-		
-		JLabel lblReportCategory = new JLabel("Kategori");
-		lblReportCategory.setBounds(10, 45, 48, 14);
-		panelReport.add(lblReportCategory);
-		
-		JLabel lblReportSupplier = new JLabel("Leverant\u00F6r");
-		lblReportSupplier.setBounds(10, 74, 72, 14);
-		panelReport.add(lblReportSupplier);
-		
-		JComboBox comboBoxReportSupplier = new JComboBox();
-		comboBoxReportSupplier.setBounds(92, 74, 105, 18);
-		panelReport.add(comboBoxReportSupplier);
-		
-		JComboBox comboBoxReportCategory = new JComboBox();
-		comboBoxReportCategory.setBounds(92, 42, 105, 19);
-		panelReport.add(comboBoxReportCategory);
-		
-		JButton btnMakeReport = new JButton("Framst\u00E4ll rapport");
-		btnMakeReport.setBounds(10, 124, 187, 23);
-		panelReport.add(btnMakeReport);
+		JButton btnRemoveInvoice = new JButton("Ta Bort");
+		btnRemoveInvoice.setBounds(10, 51, 89, 23);
+		panelRemoveInvoice.add(btnRemoveInvoice);
 		
 		JPanel panelSupplier = new JPanel();
 		tabbedPane.addTab("Leverant\u00F6rer", null, panelSupplier, null);
@@ -447,5 +324,163 @@ public class Application {
 		JButton btnRemoveSupplier = new JButton("Ta Bort Leverant\u00F6r");
 		btnRemoveSupplier.setBounds(10, 40, 178, 23);
 		panel.add(btnRemoveSupplier);
+		
+		JPanel panelCategory = new JPanel();
+		tabbedPane.addTab("Kategorier och Varor", null, panelCategory, null);
+		panelCategory.setLayout(null);
+		
+		JTabbedPane tabbedPaneInsideCategory = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPaneInsideCategory.setBounds(10, 11, 822, 489);
+		panelCategory.add(tabbedPaneInsideCategory);
+		
+		JPanel panelRegisterNewCategory = new JPanel();
+		tabbedPaneInsideCategory.addTab("Ny Kategori eller Vara", null, panelRegisterNewCategory, null);
+		panelRegisterNewCategory.setLayout(null);
+		
+		JLabel lblNewCategory = new JLabel("Ny kategori: ");
+		lblNewCategory.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewCategory.setBounds(10, 11, 79, 14);
+		panelRegisterNewCategory.add(lblNewCategory);
+		
+		JLabel lblNewCategoryName = new JLabel("Namn");
+		lblNewCategoryName.setBounds(10, 36, 48, 14);
+		panelRegisterNewCategory.add(lblNewCategoryName);
+		
+		textFieldNewCategoryName = new JTextField();
+		textFieldNewCategoryName.setBounds(78, 33, 96, 20);
+		panelRegisterNewCategory.add(textFieldNewCategoryName);
+		textFieldNewCategoryName.setColumns(10);
+		
+		JButton btnAddCategory = new JButton("L\u00E4gg till kategori");
+		btnAddCategory.setBounds(10, 61, 164, 23);
+		panelRegisterNewCategory.add(btnAddCategory);
+		
+		JLabel lblNewProduct = new JLabel("Ny vara:");
+		lblNewProduct.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewProduct.setBounds(10, 120, 48, 14);
+		panelRegisterNewCategory.add(lblNewProduct);
+		
+		JLabel lblNewProductName = new JLabel("Namn");
+		lblNewProductName.setBounds(10, 145, 48, 14);
+		panelRegisterNewCategory.add(lblNewProductName);
+		
+		JLabel lblNewProductCategory = new JLabel("Kategori");
+		lblNewProductCategory.setBounds(10, 221, 48, 14);
+		panelRegisterNewCategory.add(lblNewProductCategory);
+		
+		JComboBox comboBoxNewProductCategory = new JComboBox();
+		comboBoxNewProductCategory.setBounds(78, 219, 96, 18);
+		panelRegisterNewCategory.add(comboBoxNewProductCategory);
+		
+		textFieldNewProductName = new JTextField();
+		textFieldNewProductName.setBounds(78, 142, 96, 20);
+		panelRegisterNewCategory.add(textFieldNewProductName);
+		textFieldNewProductName.setColumns(10);
+		
+		JButton btnAddProduct = new JButton("L\u00E4gg till vara");
+		btnAddProduct.setBounds(10, 258, 164, 23);
+		panelRegisterNewCategory.add(btnAddProduct);
+		
+		textFieldNewProductNumber = new JTextField();
+		textFieldNewProductNumber.setBounds(78, 167, 96, 20);
+		panelRegisterNewCategory.add(textFieldNewProductNumber);
+		textFieldNewProductNumber.setColumns(10);
+		
+		JLabel lblVarunummer = new JLabel("Varunummer");
+		lblVarunummer.setBounds(10, 170, 79, 14);
+		panelRegisterNewCategory.add(lblVarunummer);
+		
+		JLabel lblNewProductSupplier = new JLabel("Leverant\u00F6r");
+		lblNewProductSupplier.setBounds(10, 196, 79, 14);
+		panelRegisterNewCategory.add(lblNewProductSupplier);
+		
+		JComboBox comboBoxNewProductSupplier = new JComboBox();
+		comboBoxNewProductSupplier.setBounds(78, 195, 96, 18);
+		panelRegisterNewCategory.add(comboBoxNewProductSupplier);
+		
+		JPanel panelRemoveCategory = new JPanel();
+		tabbedPaneInsideCategory.addTab("Ta Bort Kategori eller Vara", null, panelRemoveCategory, null);
+		panelRemoveCategory.setLayout(null);
+		
+		JLabel lblRemoveCategory = new JLabel("Kategori: ");
+		lblRemoveCategory.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblRemoveCategory.setBounds(17, 11, 99, 14);
+		panelRemoveCategory.add(lblRemoveCategory);
+		
+		JLabel lblRemoveCategoryName = new JLabel("Namn");
+		lblRemoveCategoryName.setBounds(17, 36, 48, 14);
+		panelRemoveCategory.add(lblRemoveCategoryName);
+		
+		JComboBox comboBoxRemoveCategoryName = new JComboBox();
+		comboBoxRemoveCategoryName.setBounds(85, 36, 99, 18);
+		panelRemoveCategory.add(comboBoxRemoveCategoryName);
+		
+		JLabel lblRemoveProduct = new JLabel("Vara:");
+		lblRemoveProduct.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblRemoveProduct.setBounds(17, 99, 73, 14);
+		panelRemoveCategory.add(lblRemoveProduct);
+		
+		lblRemoveProductName = new JLabel("Namn");
+		lblRemoveProductName.setBounds(17, 124, 48, 14);
+		panelRemoveCategory.add(lblRemoveProductName);
+		
+		JComboBox comboBoxRemoveProductName = new JComboBox();
+		comboBoxRemoveProductName.setBounds(85, 124, 99, 18);
+		panelRemoveCategory.add(comboBoxRemoveProductName);
+		
+		JButton btnTaBort_1 = new JButton("Ta Bort");
+		btnTaBort_1.setBounds(17, 153, 89, 23);
+		panelRemoveCategory.add(btnTaBort_1);
+		
+		JLabel lblEller = new JLabel("Eller");
+		lblEller.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblEller.setBounds(17, 74, 48, 14);
+		panelRemoveCategory.add(lblEller);
+		
+		JPanel panelReport = new JPanel();
+		tabbedPane.addTab("Rapport", null, panelReport, null);
+		panelReport.setLayout(null);
+		
+		JScrollPane scrollPaneReport = new JScrollPane();
+		scrollPaneReport.setBounds(388, 11, 444, 489);
+		panelReport.add(scrollPaneReport);
+		
+		JLabel lblReportChooseDate = new JLabel("V\u00E4lj datum");
+		lblReportChooseDate.setBounds(10, 17, 72, 14);
+		panelReport.add(lblReportChooseDate);
+		
+		JLabel lblReportDateTo = new JLabel("till");
+		lblReportDateTo.setBounds(165, 17, 16, 14);
+		panelReport.add(lblReportDateTo);
+		
+		JLabel lblReportCategory = new JLabel("Kategori");
+		lblReportCategory.setBounds(10, 45, 48, 14);
+		panelReport.add(lblReportCategory);
+		
+		JLabel lblReportSupplier = new JLabel("Leverant\u00F6r");
+		lblReportSupplier.setBounds(10, 74, 72, 14);
+		panelReport.add(lblReportSupplier);
+		
+		JComboBox comboBoxReportSupplier = new JComboBox();
+		comboBoxReportSupplier.setBounds(92, 74, 105, 18);
+		panelReport.add(comboBoxReportSupplier);
+		
+		JComboBox comboBoxReportCategory = new JComboBox();
+		comboBoxReportCategory.setBounds(92, 42, 105, 19);
+		panelReport.add(comboBoxReportCategory);
+		
+		JButton btnMakeReport = new JButton("Framst\u00E4ll rapport");
+		btnMakeReport.setBounds(10, 124, 187, 23);
+		panelReport.add(btnMakeReport);
+		
+		JLabel lblReportRequiredField = new JLabel("*Obligatoriskt f\u00E4lt");
+		lblReportRequiredField.setForeground(Color.RED);
+		lblReportRequiredField.setBounds(10, 158, 118, 14);
+		panelReport.add(lblReportRequiredField);
+		
+		JLabel label = new JLabel("*");
+		label.setForeground(Color.RED);
+		label.setBounds(2, 17, 49, 14);
+		panelReport.add(label);
 	}
 }
