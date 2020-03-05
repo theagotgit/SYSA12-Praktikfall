aed5c7c7a23afb94e8d9917be3fb476545784cda
th3735da-s <th3735da-s@student.lu.se>
2020-03-05 09:19:26 +0100

    Added panes to the GUI

--- src/application/Application.java
+++ src/application/Application.java
@@ -3,6 +3,9 @@ package application;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class Application {
 
@@ -36,8 +39,32 @@ public class Application {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 879, 596);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 11, 847, 539);
+		frame.getContentPane().add(tabbedPane);
+		
+		JPanel panelStart = new JPanel();
+		tabbedPane.addTab("Start", null, panelStart, null);
+		panelStart.setLayout(null);
+		
+		JPanel panelRegister = new JPanel();
+		tabbedPane.addTab("Registrera", null, panelRegister, null);
+		panelRegister.setLayout(null);
+		
+		JPanel panelReport = new JPanel();
+		tabbedPane.addTab("Rapport", null, panelReport, null);
+		panelReport.setLayout(null);
+		
+		JPanel panelSupplier = new JPanel();
+		tabbedPane.addTab("Leverant\u00F6rer", null, panelSupplier, null);
+		panelSupplier.setLayout(null);
+		
+		JPanel panelSearchInvoice = new JPanel();
+		tabbedPane.addTab("S\u00F6k faktura", null, panelSearchInvoice, null);
+		panelSearchInvoice.setLayout(null);
 	}
-
 }
