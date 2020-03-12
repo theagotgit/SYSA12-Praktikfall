e4959a1760eb74c15cd38fdceced7e873e923b11
Valdemar <56624331+Valdemar99@users.noreply.github.com>
2020-03-12 10:43:19 +0100

    Category class with addProduct
    This class now has methods and attributes.

--- src/model/Category.java
+++ src/model/Category.java
@@ -1,5 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Category {
+	private String name;
+	private ArrayList<Product> products = new ArrayList<Product>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+
+	public void addProduct(Product product) {
+		products.add(product);
+	}
 
 }
