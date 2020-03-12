e9818f3d9fab03059fdd9b2d33b8133bb3d2425a
Valdemar <56624331+Valdemar99@users.noreply.github.com>
2020-03-12 10:35:28 +0100

    Created CategoryRegister
    Now complete with methods and ArrayList.

--- src/model/CategoryRegister.java
+++ src/model/CategoryRegister.java
@@ -1,5 +1,38 @@
 package model;
 
-public class CategoryRegister {
+import java.util.ArrayList;
 
+public class CategoryRegister {
+	private ArrayList<Category> categories = new ArrayList<Category>();
+
+	public ArrayList<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(ArrayList<Category> categories) {
+		this.categories = categories;
+	}
+
+	public void addCategory(Category category) {
+		categories.add(category);
+	}
+	public Category findCategory(String name) {
+		for (Category tmp : categories) {
+			if (tmp.getName().equals(name)) {
+				return tmp;
+			}
+		}
+		return null;
+	}
+	public Category deleteCategory(String categoryName) {
+		Category category = this.findCategory(categoryName);
+		categories.remove(category);
+		return category;
+	}
+	public Category editCategory(String oldCategoryName, String newCategoryName) {
+		Category category = this.findCategory(oldCategoryName);
+		category.setName(newCategoryName);
+		return category;
+	}
 }
+
