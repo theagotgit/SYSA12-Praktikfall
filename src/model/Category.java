c21dce43b2eff28238418832cf56a1c258a9e8b4
Valdemar <56624331+Valdemar99@users.noreply.github.com>
2020-03-12 10:48:09 +0100

    Created custom constructor

--- src/model/Category.java
+++ src/model/Category.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class Category {
 	private String name;
 	private ArrayList<Product> products = new ArrayList<Product>();
+	
+	//Custom constructor
+	public Category(String name) {
+		this.name = name;
+	}
 
 	public String getName() {
 		return name;
