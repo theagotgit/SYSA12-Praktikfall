58f0a246266c2efbe5947a276000c0555a284a9a
awmelia <56584822+awmelia@users.noreply.github.com>
2020-03-12 09:46:56 +0100

    Skapade InvoiceRegister
    Skrev in get/set samt add och find. Ya'll welcome.

--- src/model/InvoiceRegister.java
+++ src/model/InvoiceRegister.java
@@ -1,5 +1,25 @@
 package model;
+import java.util.*;
 
 public class InvoiceRegister {
-
+	private ArrayList<Invoice> invoices = new ArrayList<Invoice>();
+	
+	public ArrayList<Invoice> getInvoice() {
+		return this.invoices;
+	}
+	public void setInvoice(ArrayList<Invoice> invoices) {
+		this.invoices = invoices;
+	}
+	public void addInvoice(Invoice invoice) {
+		this.invoices.add(invoice);
+	}
+	public Invoice findInvoice(String invoiceNumber) {
+		for(Invoice tmp: invoices) {
+			if(tmp.getInvoiceNumber().equals(invoiceNumber)){
+				return tmp;
+			}	
+		}
+		return null;
+	}
 }
+
