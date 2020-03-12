9870c8eeaeb77141f68a68f1aa52a30507189292
VickyYaoMing <56544221+VickyYaoMing@users.noreply.github.com>
2020-03-12 10:32:56 +0100

    Hello
    hi

--- src/model/Supplier.java
+++ src/model/Supplier.java
@@ -2,4 +2,40 @@ package model;
 
 public class Supplier {
 
+	private String name;
+	private String faxNumber;
+	private String telephoneNumber;
+	private String webAddress;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFaxNumber() {
+		return faxNumber;
+	}
+	public void setFaxNumber(String faxNumber) {
+		this.faxNumber = faxNumber;
+	}
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+	public String getWebAddress() {
+		return webAddress;
+	}
+	public void setWebAddress(String webAddress) {
+		this.webAddress = webAddress;
+	}
+	
+	
+	
+
+
+
+
 }
