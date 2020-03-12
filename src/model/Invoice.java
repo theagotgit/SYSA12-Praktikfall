dd9f5e68cd8ec148ee79dd92ccacf802be2e2a94
awmelia <56584822+awmelia@users.noreply.github.com>
2020-03-12 10:16:28 +0100

    Invoice klass klar
    Jag har lagt in alla setters och getters för alla attribut i klassen.

--- src/model/Invoice.java
+++ src/model/Invoice.java
@@ -10,10 +10,49 @@ public class Invoice {
 	private Calendar deliveryDate;
 	
 	public Invoice(String invoiceNumber, Calendar expiryDate, Calendar printedDate, Calendar acknowledgementDate, Calendar deliverydate) {
+		this.setInvoiceNumber(invoiceNumber);
+		this.setExpiryDate(expiryDate);
+		this.setPrintedDate(printedDate);
+		this.setAcknowledgementDate(acknowledgementDate);
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getInvoiceNumber() {
+		return invoiceNumber;
+	}
+
+	public void setInvoiceNumber(String invoiceNumber) {
 		this.invoiceNumber = invoiceNumber;
+	}
+
+	public Calendar getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(Calendar expiryDate) {
 		this.expiryDate = expiryDate;
+	}
+
+	public Calendar getPrintedDate() {
+		return printedDate;
+	}
+
+	public void setPrintedDate(Calendar printedDate) {
 		this.printedDate = printedDate;
+	}
+
+	public Calendar getAcknowledgementDate() {
+		return acknowledgementDate;
+	}
+
+	public void setAcknowledgementDate(Calendar acknowledgementDate) {
 		this.acknowledgementDate = acknowledgementDate;
+	}
+	public Calendar getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(Calendar deliveryDate) {
 		this.deliveryDate = deliveryDate;
 	}
 }
